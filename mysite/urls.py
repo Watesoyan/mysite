cfb455be92082ae11a8b496bde60ba33b67d08f3
Wate <31439027+Watesoyan@users.noreply.github.com>
2020-05-03 12:58:08 +0800

    demo_0

---
+++ mysite/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('blog/', include('blog.urls')),
+    path('comment/', include('comment.urls')),
+    path('likes/', include('likes.urls')),
+    path('user/', include('user.urls')),
+    path('ckeditor', include('ckeditor_uploader.urls')),
+    path('favicon.ico', serve, {'path': 'favicon.ico'}),
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
