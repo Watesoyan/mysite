7ae8311df879e20186ccc06ed2799da7ca91d15b
Wate <31439027+Watesoyan@users.noreply.github.com>
2020-05-04 16:00:38 +0800

    demo_2

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.views import serve
 
 from . import views
 
@@ -14,7 +13,6 @@ urlpatterns = [
     path('likes/', include('likes.urls')),
     path('user/', include('user.urls')),
     path('ckeditor', include('ckeditor_uploader.urls')),
-    path('favicon.ico', serve, {'path': 'favicon.ico'}),
     path('admin/', admin.site.urls),
 ]
 
